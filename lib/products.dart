33bea663a666212c574d5c33acddea7f52f2f8a9
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-01-24 19:17:23 +0330

    Add Address and decorate it

--- lib/products.dart
+++ lib/products.dart
@@ -11,34 +11,41 @@ class Products extends StatelessWidget {
         children: <Widget>[
           Image.asset(products[index]["image"]),
           Container(
-              margin: EdgeInsets.only(top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    products[index]["title"],
-                    style: TextStyle(
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Oswald"),
+            margin: EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  products[index]["title"],
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Oswald"),
+                ),
+                SizedBox(
+                  width: 8.0,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                  SizedBox(
-                    width: 8.0,
+                  child: Text(
+                    "\$ ${products[index]["price"]}",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  Container(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Text(
-                      "\$ ${products[index]["price"]}",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
-              )),
+                )
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 2.5, horizontal: 6.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text("Union Square, San Francisco"),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
