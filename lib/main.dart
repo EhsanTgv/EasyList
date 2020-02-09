c7c425bdfa99cc03e8430606164f5275ff08389e
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-09 22:56:38 +0330

    Providing the Scoped Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-import 'models/product.dart';
 import 'pages/authentication.dart';
 import 'pages/product.dart';
 import 'pages/product_admin.dart';
 import 'pages/products.dart';
+import 'scoped-models/products.dart';
 
 void main() {
 //  debugPaintSizeEnabled = true;
@@ -23,38 +24,41 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(context) {
-    return MaterialApp(
+    return ScopedModel<ProductsModel>(
+      model: ProductsModel(),
+      child: MaterialApp(
 //      debugShowMaterialGrid: true,
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-          buttonColor: Colors.deepPurple),
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primarySwatch: Colors.deepOrange,
+            accentColor: Colors.deepPurple,
+            buttonColor: Colors.deepPurple),
 //      home: AuthenticationPage(),
-      routes: {
-        '/': (BuildContext context) => AuthenticationPage(),
-        '/products': (BuildContext context) => ProductsPage(),
-        '/admin': (BuildContext context) => ProductAdminPage(),
-      },
-      onGenerateRoute: (RouteSettings settings) {
-        final List<String> pathElements = settings.name.split('/');
-        if (pathElements[0] != '') {
+        routes: {
+          '/': (BuildContext context) => AuthenticationPage(),
+          '/products': (BuildContext context) => ProductsPage(),
+          '/admin': (BuildContext context) => ProductAdminPage(),
+        },
+        onGenerateRoute: (RouteSettings settings) {
+          final List<String> pathElements = settings.name.split('/');
+          if (pathElements[0] != '') {
+            return null;
+          }
+          if (pathElements[1] == "product") {
+            final int index = int.parse(pathElements[2]);
+            return MaterialPageRoute<bool>(
+              builder: (BuildContext context) =>
+                  ProductPage(null, null, null, null),
+            );
+          }
           return null;
-        }
-        if (pathElements[1] == "product") {
-          final int index = int.parse(pathElements[2]);
-          return MaterialPageRoute<bool>(
-            builder: (BuildContext context) =>
-                ProductPage(null, null, null, null),
+        },
+        onUnknownRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+            builder: (BuildContext context) => ProductsPage(),
           );
-        }
-        return null;
-      },
-      onUnknownRoute: (RouteSettings settings) {
-        return MaterialPageRoute(
-          builder: (BuildContext context) => ProductsPage(),
-        );
-      },
+        },
+      ),
     );
   }
 }
