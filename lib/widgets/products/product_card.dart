00f74eefd468a0c6e7b50d3c9522fe7ae3926f0b
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-11 00:13:19 +0330

    Working on the Favorite Feature

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/product.dart';
-import 'package:flutter_app/widgets/products/address_tag.dart';
-import 'package:flutter_app/widgets/ui_elements/title_default.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 import 'price_tag.dart';
+import 'address_tag.dart';
+import '../ui_elements/title_default.dart';
+import '../../models/product.dart';
+import '../../scoped-models/products.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -37,10 +39,19 @@ class ProductCard extends StatelessWidget {
           onPressed: () => Navigator.pushNamed<bool>(
               context, "/product/" + productIndex.toString()),
         ),
-        IconButton(
-            icon: Icon(Icons.favorite_border),
-            color: Colors.red,
-            onPressed: () {}),
+        ScopedModelDescendant<ProductsModel>(
+          builder: (BuildContext context, Widget child, ProductsModel model) {
+            return IconButton(
+                icon: Icon(model.products[productIndex].isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () {
+                  model.selectProduct(productIndex);
+                  model.toggleProductFavoriteStatus();
+                });
+          },
+        )
       ],
     );
   }
