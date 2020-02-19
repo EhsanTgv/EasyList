07c0840da89250de47cff884c29abe559679a8b7
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-19 23:13:29 +0330

    Adding fadein to the Image Placeholder

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -61,7 +61,12 @@ class ProductCard extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
-          Image.network(product.image),
+          FadeInImage(
+            image: NetworkImage(product.image),
+            height: 300.0,
+            fit: BoxFit.cover,
+            placeholder: AssetImage("assets/food.jpg"),
+          ),
           _buildTitlePriceRow(),
           AddressTag("Union Square, San Francisco"),
           Text(product.userEmail),
