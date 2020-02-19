5879b0928c8e185435b5e93d604ab6e6d43b1df1
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-19 23:51:10 +0330

    Adjusting the Scoped Model, product page fixed

--- lib/widgets/products/product_card.dart
+++ lib/widgets/products/product_card.dart
@@ -30,18 +30,18 @@ class ProductCard extends StatelessWidget {
   }
 
   Widget _buildActionButtons(BuildContext context) {
-    return ButtonBar(
-      alignment: MainAxisAlignment.center,
-      children: <Widget>[
-        IconButton(
-          icon: Icon(Icons.info),
-          color: Theme.of(context).accentColor,
-          onPressed: () => Navigator.pushNamed<bool>(
-              context, "/product/" + productIndex.toString()),
-        ),
-        ScopedModelDescendant<MainModel>(
-          builder: (BuildContext context, Widget child, MainModel model) {
-            return IconButton(
+    return ScopedModelDescendant<MainModel>(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        return ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info),
+                color: Theme.of(context).accentColor,
+                onPressed: () => Navigator.pushNamed<bool>(
+                    context, "/product/" + model.allProducts[productIndex].id),
+              ),
+              IconButton(
                 icon: Icon(model.allProducts[productIndex].isFavorite
                     ? Icons.favorite
                     : Icons.favorite_border),
@@ -49,10 +49,10 @@ class ProductCard extends StatelessWidget {
                 onPressed: () {
                   model.selectProduct(model.allProducts[productIndex].id);
                   model.toggleProductFavoriteStatus();
-                });
-          },
-        )
-      ],
+                },
+              ),
+            ]);
+      },
     );
   }
 
