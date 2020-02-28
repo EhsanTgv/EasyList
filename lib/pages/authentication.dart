2e300231b06f9ff2890c1e5cde31702be595d3fa
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-28 15:56:32 +0330

    AuthenticationPage Confirm Password Added

--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -18,6 +18,7 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
     'acceptTerms': false
   };
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordTextController = TextEditingController();
 
   DecorationImage _buildBackgroundImage() {
     return DecorationImage(
@@ -51,6 +52,7 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
       decoration: InputDecoration(
           labelText: 'Password', filled: true, fillColor: Colors.white),
       obscureText: true,
+      controller: _passwordTextController,
       validator: (String value) {
         if (value.isEmpty || value.length < 6) {
           return 'Password invalid';
@@ -58,6 +60,22 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
       },
       onSaved: (String value) {
         _formData['password'] = value;
+      },
+    );
+  }
+
+  Widget _buildEmailConfirmTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Confirm Password', filled: true, fillColor: Colors.white),
+      obscureText: true,
+      validator: (String value) {
+        if (_passwordTextController.text != value) {
+          return 'Passwords do not match.';
+        }
+      },
+      onSaved: (String value) {
+        _formData['email'] = value;
       },
     );
   }
@@ -109,7 +127,15 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
                       height: 10.0,
                     ),
                     _buildPasswordTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _buildEmailConfirmTextField(),
                     _buildAcceptSwitch(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    
                     SizedBox(
                       height: 10.0,
                     ),
